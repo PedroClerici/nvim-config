54482d2dafa6bfda55d9fe1702e1a1a92a5315e5
Pedro Clerici <pedro.clerici@protonmail.com>
2024-01-01 10:38:50 -0300

    Changed stylua configs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
       "nvim-telescope/telescope-ui-select.nvim",
     },
     config = function()
-      require("telescope").setup {
+      require("telescope").setup({
         defaults = {
           mappings = {
             i = {
@@ -21,12 +21,12 @@ return {
             require("telescope.themes").get_dropdown(),
           },
         },
-      }
+      })
 
-      require("telescope").load_extension "ui-select"
+      require("telescope").load_extension("ui-select")
 
       -- Keymaps
-      local builtin = require "telescope.builtin"
+      local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files)
       vim.keymap.set("n", "<leader>fg", builtin.live_grep)
       vim.keymap.set("n", "<leader>fb", builtin.buffers)
